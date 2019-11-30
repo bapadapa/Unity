7b9288f8611ccf3ee919fff858dda56533366701
bapadapa <painclown@gmail.com>
2019-11-30 21:43:56 +0900

    nav

--- Assets/Scripts/Game/CameraController.cs
+++ Assets/Scripts/Game/CameraController.cs
@@ -3,18 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour {
-    //public GameObject player;
+    public GameObject player;
 
-    //public float offsetX = 0f;
-    //public float offsetY = 5f;
-    //public float offsetZ = -10f;
-    //public float followSpeed = 100f;   
-    //Vector3 cameraPosition;
+    public float offsetX = 0f;
+    public float offsetY = 5f;
+    public float offsetZ = -10f;
+    public float followSpeed = 100f;
+    Vector3 cameraPosition;
 
-    public Transform targetTransform; // Player의 Transform 컴포넌트.
-    public float dist = 3.0f;  //거리
-    public float height = 5.0f;  //높이
-    public float dampTrace = 20.0f;  //추적 속도.
+    //public Transform targetTransform; // Player의 Transform 컴포넌트.
+    //public float dist = 3.0f;  //거리
+    //public float height = 5.0f;  //높이
+    //public float dampTrace = 20.0f;  //추적 속도.
 
     private Transform _transform; // Camera Transform 컴포넌트
 
@@ -31,17 +31,17 @@ public class CameraController : MonoBehaviour {
     //다른 Update문을 실행하고 마지막에 실행되는 Update사이클
     void LateUpdate()
     {
-        //cameraPosition.x = player.transform.position.x + offsetX;
-        //cameraPosition.y = player.transform.position.y + offsetY;
-        //cameraPosition.z = player.transform.position.z + offsetZ;
+        cameraPosition.x = player.transform.position.x + offsetX;
+        cameraPosition.y = player.transform.position.y + offsetY;
+        cameraPosition.z = player.transform.position.z + offsetZ;
 
 
-        //transform.position = cameraPosition;
+        transform.position = cameraPosition;
         //위 함수를 조금 더 부드럽게 만들기.
         //transform.position = Vector3.Lerp(transform.position, cameraPosition, followSpeed * Time.deltaTime);
 
-        _transform.position = Vector3.Lerp(_transform.position, targetTransform.position - (targetTransform.forward * dist) + (Vector3.up * height), Time.deltaTime * dampTrace);
-        
-        _transform.LookAt(targetTransform.position);
+        //_transform.position = Vector3.Lerp(_transform.position, targetTransform.position - (targetTransform.forward * dist) + (Vector3.up * height), Time.deltaTime * dampTrace);
+
+        //_transform.LookAt(targetTransform.position);
     }
 }
--- Assets/Scripts/Game/SmallEnemy.cs
+++ Assets/Scripts/Game/SmallEnemy.cs
@@ -1,18 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class SmallEnemy : MonoBehaviour
 {
+
+    private Transform _transform;
+    private Transform playerTransform;
+    private NavMeshAgent nvAgent;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _transform = this.gameObject.GetComponent<Transform>();
+        playerTransform = GameObject.FindWithTag("Player").GetComponent<Transform>();
+        nvAgent = this.gameObject.GetComponent<NavMeshAgent>();
+        nvAgent.destination = playerTransform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+
     }
 }
