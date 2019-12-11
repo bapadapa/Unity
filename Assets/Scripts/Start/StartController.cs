842f119dfb63aa967c2c0af785a49b8200f96e13
bapadapa <painclown@gmail.com>
2019-12-11 23:57:17 +0900

    2학기 마지막 작업

--- Assets/Scripts/Start/StartController.cs
+++ Assets/Scripts/Start/StartController.cs
@@ -2,17 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 public class StartController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+
+
+    // Use this for initialization
+    void Start () {
+}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+
+    }
 
     public void ChangeToGameScene()
     {
@@ -20,5 +22,10 @@ public class StartController : MonoBehaviour {
         Debug.Log("Start");
         UnityEngine.SceneManagement.SceneManager.LoadScene("GameScenes");
         
+    }
+
+    public void EndGame()
+    {
+        Application.Quit();
     }
 }
